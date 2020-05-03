e591e890e4e483415d3ccffe627f39ecd639b80d
chen001 <2025077366@qq.com>
2020-05-03 23:12:46 +0800

    登录页面修改

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 /*应用根组件*/
 import React,{Component} from 'react'
-export default class App extends Component{
+import {Button,message} from 'antd'
+import { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom"
+import Login from "./pages/login/login.jsx"
+import Admin from "./pages/admin/admin.jsx"
+ class App extends Component{
+  handleClick=()=>{
+    message.success('成功啦。。。');
+  }
   render(){
+   
     return (
+      <HashRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/admin" component={Admin} />
+
+        </Switch>
+
+
+      </HashRouter>
       
-      <div>App</div>
         
     )
 
   }
 
 }
+
+
+
+
+export default App
